48e1b9da9dd5af2e29e008c32ee9140ee83947c2
ming-afk <83766133+ming-afk@users.noreply.github.com>
2022-06-07 22:20:50 -0400

    added another grep command

--- main.py
+++ main.py
@@ -8,14 +8,32 @@ import subprocess
 import time
 from datetime import datetime as dt
 
+def save_xlsx(update_df):
+    try:
+        cur_df = pd.read_excel('data/repo_updates.xlsx')
+        cont_df = pd.concat([cur_df, update_df], ignore_index = True)
+        cont_df.to_excel('data/repo_updates.xlsx')
+    except FileNotFoundError:
+        update_df.to_excel('data/repo_updates.xlsx')
+    return
+
+
+def save_csv(update_df):
+    try:
+        cur_df = pd.read_csv('data/repo_updates.csv')
+        cont_df = pd.concat([cur_df, update_df], ignore_index = True)
+        cont_df.to_csv('data/repo_updates.csv')
+    except pd.errors.EmptyDataError:
+        update_df.to_csv('data/repo_updates.csv')
+    return
 
 def save_json(update_di):
     print('called')
     start = dt.now()
     jstring = json.dumps(update_di, indent=4, sort_keys=True, default=str)
 
-    # with open("../data/repo_updates.json", "w+") as fp:
-    with open("../data/repo_updates.json", "a") as fp:
+    # with open("../data/tensorflow_repo_updates.json", "w+") as fp:
+    with open("data/repo_updates.json", "a") as fp:
         fp.write(jstring)
         fp.close()
     print("saving alone took: ", dt.now()-start, ' seconds')
@@ -63,19 +81,22 @@ def print_fields():
 
 
 if __name__ == "__main__":
-    #
-    # start = dt.now()
-    # # Repository('https://github.com/tensorflow/tensorflow', clone_repo_to=True)
-    # print()
-    # print("driller cloning took: ", dt.now()-start)
 
-    repos = pd.read_csv('data/GitHub_sorted_issue_commit_all.csv', nrows=5)['repository']
-    # for repo in repos:
 
-    # update_df = pd.DataFrame({key: [] for key in ['hash', 'lines']})
-    repos = ['tensorflow/tensorflow']
+    repos = pd.read_csv('data/GitHub_sorted_issue_commit_all.csv', nrows=1)['repository']
+
+    # repos = ['ming-afk/Octlearner']
+
+    update_df = pd.DataFrame({key:[] for key in ['repository', 'project_name', 'msg', 'hash', 'lines', 'file', 'project_path', 'insertions',\
+                              'deletions', 'in_main_branch', 'files', 'author_date',\
+                              'author_timezone', 'author_name', 'author_email',\
+                              'committer_date', 'committer_timezone', 'committer_name',\
+                              'committer_email', 'merge', 'branches']})
+
+    save_wait = 2 # wait for another repo before saving
 
     for repo in repos:
+
     # getting the repositroy
 
         start = dt.now()
@@ -99,50 +120,89 @@ if __name__ == "__main__":
             res = line.replace('\t',' ').replace('\n','').split(' ')
             paths.append(' '.join(res[3:]))
 
-        print(paths)
         # checking file content with below command
-        os.system('grep -i -E "code\s*_?-?of\s*_?-?conduct" * > ../out.txt')
+        # "git grep -l "code of conduct""
+        os.system('git grep -l -i -E "code\s*_?-?of\s*_?-?conduct"> ../out.txt')
         f = open("../out.txt", "r")
 
-        cont_res = f.read()
+        cont_res = f.readlines()
+        for i in cont_res:
+            paths.append(i.replace('\t',' ').replace('\n',''))
+        print(paths)
 
-        # print(cont_res)
         # cont_res = cont
         # assumption for string matching:
         #   1. that the file path will end in: ".file_extension:"
 
         start = dt.now()
         for path in paths:
-            print(path)
+            print('current file path is: ', path)
             for r in Repository('.', filepath=path).traverse_commits():
 
                 update_di = {'repository': repo, 'hash': r.hash, 'lines':r.lines, 'project_name':r.project_name,\
-                              'msg':r.msg, 'project_path':r.project_path, 'parents':r.parents, 'insertions':r.insertions,\
+                              'msg':r.msg, 'project_path': r.project_path, 'insertions':r.insertions,\
                               'deletions': r.deletions, 'in_main_branch':r.in_main_branch, 'files':r.files, 'author_date':r.author_date,\
                               'author_timezone': r.author_timezone, 'author_name':r.author.name, 'author_email':r.author.email,\
                               'committer_date': r.committer_date, 'committer_timezone':r.committer_timezone,\
                               'committer_email': r.committer.email, 'committer_name':r.committer.name, 'merge':r.merge,\
-                              'branches': r.branches}
+                              'branches': str(r.branches).replace('{','').replace('}','')}
 
                 files = list()
 
                 for i in range(len(r.modified_files)):
 
-                    file_di = {'old_path': r.modified_files[i].old_path, \
-                            'new_path': r.modified_files[i].new_path, 'filename': r.modified_files[i].filename, \
-                            'added_lines': r.modified_files[i].added_lines, 'complexity': r.modified_files[i].complexity, \
-                            'deleted_lines': r.modified_files[i].deleted_lines, 'diff_parsed': r.modified_files[i].diff_parsed, \
-                            'language_supported': r.modified_files[i].language_supported, 'nloc': r.modified_files[i].nloc, \
-                            'token_count': r.modified_files[i].token_count}
+                    ifile = r.modified_files[i]
 
+                    file_di = {'old_path': ifile.old_path, 'complexity':ifile.complexity,\
+                            'new_path': ifile.new_path, 'filename': ifile.filename, \
+                            'added_lines': ifile.added_lines, \
+                            'deleted_lines': ifile.deleted_lines, 'diff_parsed': ifile.diff_parsed, \
+                            'language_supported': ifile.language_supported, 'nloc': ifile.nloc, \
+                            'token_count': ifile.token_count}
                     files.append(file_di)
 
-                update_di['file'] = files
-                save_json(update_di)
+                # this makes sure that files length is one
+                emp = []
+                emp.append(files)
+                update_di['file'] = emp
+                # print('length is ', len(update_di['file']))
+
+                for key, val in update_di.items():
+                    if val is None:
+                        update_di[key] = "None"
+
+                print(type(update_di))
+                # for key, val in update_di.items():
+                #     print('type of each dict val is: ', type(val))
+                #     print('the key is ', key)
+
+                cur_df = pd.DataFrame(update_di)
+
+                # cur_df = pd.DataFrame.from_dict(update_di, orient='index')
+                # cur_df = cur_df.transpose()
+                # print(cur_df)
+
+                update_df = pd.concat([update_df, cur_df])
 
         print("one repo data field extraction and saving took: ", dt.now()-start, " seconds")
 
         os.chdir("..")
         os.system('rm -rf '+repo.split('/')[1])
 
+        save_wait -= 1
+        if not save_wait:
+            save_wait = 2
+            print('saving the last two repos ... ...')
+            save_xlsx(update_df)
+            update_df = pd.DataFrame({key: [] for key in
+                                      ['repository', 'project_name', 'msg', 'hash', 'lines', 'file', 'project_path',
+                                       'insertions', \
+                                       'deletions', 'in_main_branch', 'files', 'author_date', \
+                                       'author_timezone', 'author_name', 'author_email', \
+                                       'committer_date', 'committer_timezone', 'committer_name', \
+                                       'committer_email', 'merge', 'branches']})
+
+    # save_json({'data': js_li})
+    print('all repos finished, saving .. ...')
+    save_xlsx(update_df)
 
